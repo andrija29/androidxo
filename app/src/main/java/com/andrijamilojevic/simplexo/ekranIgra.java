c5ed9921deddbf58bf1695b3c03ab754a7698d96
andrija29 <andrijamilo99@gmail.com>
2018-06-02 01:51:59 +0200

    Project created

---
+++ app/src/main/java/com/andrijamilojevic/simplexo/ekranIgra.java
@@ -0,0 +1,13 @@
+package com.andrijamilojevic.simplexo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ekranIgra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekran_igra);
+    }
+}
